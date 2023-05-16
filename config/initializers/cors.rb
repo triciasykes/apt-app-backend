66c52718d8818200431ab8bbe58302c9455be085
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-05-16 15:55:01 -0700

    updated with backend url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https//apt-app-backend.onrender.com'
     resource '*',
     headers: ["Authorization"],
     expose: ["Authorization"],
