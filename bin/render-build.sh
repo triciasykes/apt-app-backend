8e3e00b6a31ba4b09289312d75a2659ce4214d18
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-05-17 18:32:30 -0700

    minor rix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,9 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:reset
+bundle exec rake db:migrate
+bundle exec rake db:seed
+
 
 
 
