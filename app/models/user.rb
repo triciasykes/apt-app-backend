d38e0410cf226e7b64b780b24553d75e0a920bb4
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-06-06 14:54:25 -0700

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord  
-    devise :database_authenticatable, :registerable, :validatable,
-           :jwt_authenticatable, jwt_revocation_strategy: self
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
-  has_many :apartments       
+  has_many :apartments 
 end
